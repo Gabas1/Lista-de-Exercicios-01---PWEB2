ab5e7948f8cc8d2f8944d7e1afd362b338f7c26e
Gabas1 <99366961+Gabas1@users.noreply.github.com>
2023-03-21 21:49:05 -0300

    Add files via upload

---
+++ Lista de Exercícios #01/Q01/script.js
@@ -0,0 +1,8 @@
+//solved
+let x = parseInt(prompt(""));
+
+while(x != 0){
+    document.write(x * x);
+    document.write("<br>");
+    x = parseInt(prompt(""));
+}
---
+++ Lista de Exercícios #01/Q02/script.js
@@ -0,0 +1,9 @@
+//solved
+let x = parseInt(prompt("digite um valor: "));
+let soma= 0;
+
+
+while(x!=0){
+    document.write(soma = soma + x);
+    x = parseInt(prompt("digite um valor: "));
+}
